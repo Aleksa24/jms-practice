358627f2945957b6770c96d9526c5cc3899e9b82
Aleksa24 <aleksaivanovic24444@gmail.com>
2020-11-29 17:27:30 +0100

    made config for Jms and Task config, made Listener and Sender along with Message class,made send and sendAndReceive msg

---
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -0,0 +1,64 @@
+package guru.springframework.sfgjms.sender;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("HELLO WORLD")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE,message);
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("HELLO WORLD V2")
+                .build();
+
+        Message receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+
+                    System.out.println("sending hello");
+
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("boom");
+                }
+            }
+        });
+
+        System.out.println(receivedMsg.getBody(String.class));
+    }
+
+}
